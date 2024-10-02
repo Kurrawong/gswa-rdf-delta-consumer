ae3e2875824bf03f1cd82a8aa3cfafc8a5469401
Edmond Chuc <edmond@kurrawong.ai>
2024-10-02 20:43:05 +1000

    feat: add asb session lock renewal, session lock lost error, and keyboard interrupt

--- src/consumer.py
+++ src/consumer.py
@@ -2,8 +2,9 @@ import asyncio
 import argparse
 
 from loguru import logger
-from azure.servicebus.aio import ServiceBusClient, AutoLockRenewer
+from azure.servicebus.aio import ServiceBusClient
 from azure.servicebus import TransportType
+from azure.servicebus.exceptions import SessionLockLostError
 
 from gswa_rdf_delta_consumer import process_message
 from gswa_rdf_delta_consumer.settings import settings
@@ -18,19 +19,19 @@ async def main():
         settings.conn_str,
         transport_type=TransportType.AmqpOverWebsocket,
     ) as client:
-        async with client:
-            receiver = client.get_subscription_receiver(
-                topic_name=settings.topic,
-                subscription_name=settings.subscription,
-                session_id=settings.session_id,
-            )
-            renewer = AutoLockRenewer()
-            receiver._auto_lock_renewer = renewer
-            logger.info(
-                f"Connected to subscription {settings.subscription} and awaiting messages on topic {settings.topic}"
-            )
-            async with receiver:
-                while True:
+        receiver = client.get_subscription_receiver(
+            topic_name=settings.topic,
+            subscription_name=settings.subscription,
+            session_id=settings.session_id,
+        )
+        logger.info(
+            f"Connected to subscription {settings.subscription} and awaiting messages on topic {settings.topic}"
+        )
+        async with receiver:
+            while True:
+                try:
+                    logger.debug("Renewing session lock.")
+                    await receiver.session.renew_lock()
                     messages = await receiver.receive_messages(
                         max_message_count=MAX_MESSAGE_COUNT, max_wait_time=MAX_WAIT_TIME
                     )
@@ -39,6 +40,11 @@ async def main():
                         await process_message(
                             message, receiver, settings.topic, settings.session_id
                         )
+                except SessionLockLostError:
+                    logger.error("Session lock lost, renewing lock.")
+                    await receiver.session.renew_lock()
+                except Exception as err:
+                    logger.error(f"An unexpected error occurred: {err}")
 
 
 async def cli():
@@ -47,8 +53,12 @@ async def cli():
         description="An RDF Delta Consumer consuming from a subscription's topic in the Azure Service Bus.",
     )
     parser.parse_args()
+
     await main()
 
 
 if __name__ == "__main__":
-    asyncio.run(cli())
+    try:
+        asyncio.run(cli())
+    except KeyboardInterrupt:
+        logger.info("Shutting down RDF Delta Consumer")
