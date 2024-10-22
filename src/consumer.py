7e084664615887b068f1ca0f4f35354a9b1b2eed
Edmond Chuc <edmond@kurrawong.ai>
2024-10-22 12:12:05 +1000

    fix: service bus subscription eventually hangs

--- src/consumer.py
+++ src/consumer.py
@@ -3,7 +3,7 @@ import argparse
 
 from loguru import logger
 from azure.servicebus.aio import ServiceBusClient
-from azure.servicebus import TransportType
+from azure.servicebus import TransportType, ServiceBusReceiveMode
 from azure.servicebus.exceptions import SessionLockLostError
 
 from gswa_rdf_delta_consumer import process_message
@@ -12,6 +12,7 @@ from gswa_rdf_delta_consumer.settings import settings
 MAX_MESSAGE_COUNT = 1
 MAX_WAIT_TIME = 5
 TIMEOUT = 10.0
+MAX_LOCK_RENEWAL_DURATION_SEC = 100.0
 
 
 async def main():
@@ -20,26 +21,31 @@ async def main():
         settings.conn_str,
         transport_type=TransportType.AmqpOverWebsocket,
     ) as client:
-        receiver = client.get_subscription_receiver(
-            topic_name=settings.topic,
-            subscription_name=settings.subscription,
-            session_id=settings.session_id,
-            max_wait_time=TIMEOUT,
-        )
-        logger.info(
-            f"Connected to subscription {settings.subscription} and awaiting messages on topic {settings.topic}"
-        )
-        async with receiver:
-            while True:
-                try:
+
+        while True:
+            try:
+                receiver = client.get_subscription_receiver(
+                    topic_name=settings.topic,
+                    subscription_name=settings.subscription,
+                    session_id=settings.session_id,
+                    max_wait_time=TIMEOUT,
+                    receive_mode=ServiceBusReceiveMode.PEEK_LOCK,
+                )
+                logger.info(
+                    f"Connected to subscription {settings.subscription} and awaiting messages on topic {settings.topic}"
+                )
+
+                async with receiver:
                     logger.debug("Renewing session lock.")
                     await receiver.session.renew_lock(timeout=TIMEOUT)
                     logger.debug("Session lock renewed. Waiting for messages.")
+
                     messages = await asyncio.wait_for(
                         receiver.receive_messages(
-                            max_message_count=MAX_MESSAGE_COUNT, max_wait_time=MAX_WAIT_TIME
+                            max_message_count=MAX_MESSAGE_COUNT,
+                            max_wait_time=MAX_WAIT_TIME,
                         ),
-                        timeout=MAX_WAIT_TIME + 5  # Add a small buffer to the timeout
+                        timeout=MAX_WAIT_TIME + 5,  # Add a small buffer to the timeout
                     )
                     logger.debug(f"{len(messages)} messages received.")
                     for message in messages:
@@ -47,13 +53,16 @@ async def main():
                         await process_message(
                             message, receiver, settings.topic, settings.session_id
                         )
-                        logger.info(f"Message processed successfully. Message ID {message.message_id}")
-                except asyncio.TimeoutError:
-                    logger.debug("Timeout occurred while receiving messages.")
-                except SessionLockLostError:
-                    logger.debug("Session lock lost")
-                except Exception as err:
-                    logger.error(f"An unexpected error occurred: {err}")
+                        logger.info(
+                            f"Message processed successfully. Message ID {message.message_id}"
+                        )
+
+            except asyncio.TimeoutError:
+                logger.debug("Timeout occurred while receiving messages.")
+            except SessionLockLostError:
+                logger.debug("Session lock lost")
+            except Exception as err:
+                logger.error(f"An unexpected error occurred: {err}")
 
 
 async def cli():
