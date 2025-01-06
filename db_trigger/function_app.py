2c6d987bfc057135abb436dcabaea45f952823c4
Edmond <edmond.chuc@outlook.com>
2025-01-06 04:37:30 +0000

    feat: sql db trigger sends message to topic and marks row as published on success

--- db_trigger/function_app.py
+++ db_trigger/function_app.py
@@ -2,9 +2,26 @@ import json
 import logging
 
 import azure.functions as func
+from event_persistence_consumer.database import Database, EventTable
 from event_persistence_consumer.servicebus import Client
+from event_persistence_consumer.settings import settings as event_persistence_settings
 
 from db_trigger.settings import settings
+
+
+def init():
+    with Database(
+        event_persistence_settings.mssql_server,
+        event_persistence_settings.mssql_database,
+        event_persistence_settings.mssql_master_database,
+        event_persistence_settings.mssql_username,
+        event_persistence_settings.mssql_password,
+    ) as db:
+        # Create the table and set up triggers, if it does not exist.
+        EventTable(db.connection)
+
+
+init()
 
 app = func.FunctionApp()
 
@@ -18,24 +35,32 @@ app = func.FunctionApp()
 async def event_trigger(event: str) -> None:
     rows: list[dict] = json.loads(event)
     rows.sort(key=lambda x: x["Item"]["EventID"])
-    logging.info(f"Received rows: {json.dumps(rows, indent=2)}")
 
-    async with Client(
-        conn_str=settings.conn_str,
-        topic=settings.topic,
-        ws=settings.ws,
-    ) as client:
-        for row in rows:
-            event_published = row["Item"]["EventPublished"]
-            if event_published:
-                logging.info(f"Event {row['Item']['EventID']} already published")
-            else:
-                logging.info(f"Publishing event {row['Item']['EventID']}")
-                metadata = json.loads(row["Item"]["EventHeader"])
-                logging.info(type(metadata))
-                logging.info(metadata)
-                await client.send_message(
-                    session_id=settings.session,
-                    message=row["Item"]["EventBody"],
-                    metadata=metadata,
-                )
+    with Database(
+        event_persistence_settings.mssql_server,
+        event_persistence_settings.mssql_database,
+        event_persistence_settings.mssql_master_database,
+        event_persistence_settings.mssql_username,
+        event_persistence_settings.mssql_password,
+    ) as db:
+        table = EventTable(db.connection)
+
+        async with Client(
+            conn_str=settings.conn_str,
+            topic=settings.topic,
+            ws=settings.ws,
+        ) as client:
+            for row in rows:
+                event_published = row["Item"]["EventPublished"]
+                if event_published:
+                    logging.info(f"Event {row['Item']['EventID']} already published")
+                else:
+                    logging.info(f"Publishing event {row['Item']['EventID']}")
+                    metadata = json.loads(row["Item"]["EventHeader"])
+                    logging.info(metadata)
+                    await client.send_message(
+                        session_id=settings.session,
+                        message=row["Item"]["EventBody"],
+                        metadata=metadata,
+                    )
+                    table.mark_as_published(row["Item"]["EventID"])
