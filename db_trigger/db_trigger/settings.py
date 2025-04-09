0f38a45d9a27eafd38cbd14c7279f9a482d85550
Lawon Lewis <lawson@kurrawong.ai>
2025-04-09 10:20:54 +1000

    remove auto odbc driver version

--- db_trigger/db_trigger/settings.py
+++ db_trigger/db_trigger/settings.py
@@ -5,21 +5,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from db_trigger.env import env_file
 
-py_version = str(sys.version_info.major) + "." + str(sys.version_info.minor)
-if py_version == "3.10":
-    odbc_driver_string = "Driver={ODBC Driver 17 for SQL Server};"
-elif py_version == "3.11":
-    odbc_driver_string = "Driver={ODBC Driver 18 for SQL Server};"
-else:
-    raise Exception(f"odbc driver selection not implemented for python {py_version}")
-
-sql_connection_string = os.environ.get("SqlConnectionString")
-if sql_connection_string is None:
-    raise ValueError("Error: SqlConnectionString must be set")
-
-sql_connection_string_odbc = odbc_driver_string + sql_connection_string
-
-
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=env_file, env_file_encoding="utf-8", extra="allow"
@@ -29,7 +14,7 @@ class Settings(BaseSettings):
     service_bus_topic: str = ""
     service_bus_session_id: str = ""
     use_amqp_over_ws: bool = True
-    sql_connection_string_odbc: str = sql_connection_string_odbc
+    sql_connection_string = os.environ.get("SqlConnectionString", "")
 
 
 settings = Settings()
